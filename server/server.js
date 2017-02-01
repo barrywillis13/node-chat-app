70d22fb17b4b99e81c7fd74f057c753c45bc45ad
barrywillis13 <barrywillis13@gmail.com>
2017-02-01 15:33:14 +0000

    added geolocation form and input form

--- server/server.js
+++ server/server.js
@@ -22,19 +22,12 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message)
     io.emit('newMessage', generateMessage(message.from, message.text))
-    callback('This is from the sever');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
+    callback('This is from the server');
   })
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined',
-    createdAt: new Date().getTime()
-  })
+socket.on('createLocationMessage', (coords) => {
+  io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`))
+});
 
 
   socket.on('disconnect', () => {
