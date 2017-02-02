e11878a759e564f21e552eef628fbffc258af0d7
barrywillis13 <barrywillis13@gmail.com>
2017-02-02 15:07:41 +0000

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 
 var socket = io();
 
+function scrollToBottom () {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
+
 socket.on('connect', function() {
   console.log('Connected to Server')
 })
@@ -19,12 +36,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
-
-  // var formattedTime = moment(message.createdAt).format('h:mm a')
-  // var li = jQuery('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`)
-  //
-  // jQuery('#messages').append(li);
+  scrollToBottom()
 })
 
 socket.on('newLocationMessage', function (message) {
@@ -38,14 +50,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
-  // var formattedTime = moment(message.createdAt).format('h:mm a')
-  // var li = jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My Current Location</a>')
-
-  // li.text(`${message.from} ${formattedTime}:`)
-  // a.attr('href', message.url)
-  // li.append(a);
-  // jQuery('#messages').append(li);
+  scrollToBottom()
 })
 
 jQuery('#message-form').on('submit', function (e) {
@@ -76,7 +81,7 @@ locationButton.on('click', function () {
       longitude: position.coords.longitude
     });
   }, function () {
-    locationButton.removeAttr('disabled').text('Send location');;
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.')
   })
 })
